0a35b9fbc5333a951b8b65c808631155f8c91e7b
Marcus Xavier <marcusxavierr123@gmail.com>
2023-09-10 17:58:22 -0300

    feat: implement a function to help initiate the viper config

---
+++ internal/utils/viper.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"github.com/MarcusXavierr/log-auth/internal/constants"
+	"net/http"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+func InitializeViper() error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return errors.Wrap(err, "Cannot find UserHomeDir")
+	}
+
+	viper.SetConfigName(".log-auth")
+	viper.AddConfigPath(home)
+	viper.SetDefault("endpoint", "https://apiauth.logcomex.io/api/login")
+	viper.SetDefault(constants.REQUEST_METHOD, http.MethodPost)
+	viper.SetDefault(constants.SHIPMENT_INTEL_DATASET_ID, "62c3d669-c1ed-499e-bcd5-32ff1108a814")
+	viper.SetDefault(constants.FREIGHT_INTEL_DATASET_ID, "4765cb7a-b9b3-4f04-a378-a34045e31836")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return errors.Wrap(err, "ConfigFileNotFoundError")
+		}
+		return err
+	}
+
+	return nil
+}
