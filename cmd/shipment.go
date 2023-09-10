caf69aba4092ebb9893b3c479f9668801ab65652
Marcus Xavier <marcusxavierr123@gmail.com>
2023-09-10 17:59:18 -0300

    feat: create commands to get the URL for the desired platform

---
+++ cmd/shipment.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/MarcusXavierr/log-auth/internal/enum"
+	"github.com/MarcusXavierr/log-auth/internal/login"
+	"github.com/MarcusXavierr/log-auth/internal/stringgenerator"
+	"github.com/spf13/cobra"
+)
+
+var shipmentCmd = &cobra.Command{
+	Use:   "shipment",
+	Short: "Gets the local url for shipment intel",
+	Run: func(cmd *cobra.Command, args []string) {
+		url, err := stringgenerator.GeneratePBILocalUrl(login.MountLoginData(), enum.ShipmentIntel)
+
+		if err != nil {
+			fmt.Println("Error generating URL")
+			panic(err)
+		}
+
+		fmt.Println("Generated url", url)
+	},
+}
